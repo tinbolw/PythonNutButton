69fe465a59a0096ab32e7b962302a4d59a709aaa
tinbolw <tinbolw@gmail.com>
2020-06-18 10:11:03 -0700

    added gui base and nut sound effect

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import playsound
+
+root = tk.Tk()
+nutCanvas = tk.Canvas()
+nutButton = tk.Button(root, text = "Nut Button", width=10, height=5, command=())
+nutButton.pack()
+root.mainloop()
